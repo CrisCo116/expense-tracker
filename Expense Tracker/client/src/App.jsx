ad24e94017f61e594b665961473ae4b6c5349104
sefufim <132181028+sefufim@users.noreply.github.com>
2024-01-03 17:02:27 -0800

    added tailwind config file

--- Expense Tracker/client/src/App.jsx
+++ Expense Tracker/client/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
+function useBodyClass(className) {
+  useEffect(() => {
+    document.body.classList.add(className);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return () => {
+      document.body.classList.remove(className);
+    };
+  }, [className]);
 }
 
-export default App
+function App() {
+  useBodyClass('bg-zinc-500');
+
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+}
+
+export default App;
