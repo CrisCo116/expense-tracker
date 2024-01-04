299b4f501020cd994520c23be74827df7beb7cd5
Victor <frozen711.vr.vr@gmail.com>
2024-01-03 16:40:02 -0800

    added redirect to login at the bottom of the signup form

--- Expense Tracker/client/src/pages/Signup.jsx
+++ Expense Tracker/client/src/pages/Signup.jsx
@@ -31,59 +31,62 @@ export default function SignUp() {
                 <form className="space-y-6" action="#" method="POST">
                     <div>
                         <label
-                        htmlFor="email"
-                        className="block text-sm font-medium leading-6 text-gray-900"
+                            htmlFor="email"
+                            className="block text-sm font-medium leading-6 text-gray-900"
                         >
-                        Email address
+                            Email address
                         </label>
                         <div className="mt-2">
                             <input
-                            id="email"
-                            name="email"
-                            type="email"
-                            autoComplete="email"
-                            required
-                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                id="email"
+                                name="email"
+                                type="email"
+                                autoComplete="email"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
 
                     <div>
                         <label
-                        htmlFor="password"
-                        className="block text-sm font-medium leading-6 text-gray-900"
+                            htmlFor="password"
+                            className="block text-sm font-medium leading-6 text-gray-900"
                         >
-                        Password
+                            Password
                         </label>
                         <div className="mt-2">
                             <input
-                            id="password"
-                            name="password"
-                            type="password"
-                            autoComplete="current-password"
-                            required
-                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                id="password"
+                                name="password"
+                                type="password"
+                                autoComplete="current-password"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
 
                     <div>
                         <button
-                        type="submit"
-                        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
-                        Sign up
+                            Sign up
                         </button>
                     </div>
                 </form>
             
                 <p className="mt-10 text-center text-sm text-gray-500">
-                Already a member?{" "}
+                    Already a member?{" "}
                     <a
-                    href="#"
-                    className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                        href="/login" 
+                        className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                        onClick={() => {
+                            window.location.href = "/login";
+                        }}
                     >
-                    Log in
+                        Log in
                     </a>
                 </p>
             </div>
