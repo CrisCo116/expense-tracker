cc19750eeabdb368c9b45ca4c8e68b4d04f87047
Martin <mxrtinee@user.noreply.github.com>
2024-01-03 20:47:59 -0800

    Updated Footer/Home/App.jsx

---
+++ Expense Tracker/client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="bg-blue-100 mt-8 fixed w-full bottom-0">
+      <div className="container mx-auto px-4 py-4">
+        <p className="text-gray-600 text-center text-sm">
+          Copyright © 2023. All rights are reserved
+        </p>
+      </div>
+    </footer>
+  );
+}
--- Expense Tracker/client/src/pages/Home.jsx
+++ Expense Tracker/client/src/pages/Home.jsx
@@ -1,7 +1,31 @@
+import Button from '@mui/material/Button'
+
 export default function Home() {
-    return (
-      <div>
-        <h1>Home</h1>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <section className="text-center p-16">
+        <h1 className="text-3xl font-bold mb-4">Empower your financial journey with SpendSmart</h1>
+        <p className="mb-8">
+          The smarter way to budget, save, and achieve your financial goals.
+          Take the first steps towards a brighter financial future!
+        </p>
+        <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" variant="contained">
+          Create Your Free Account
+        </Button>
+      </section>
+      <section className="text-center p-16">
+        <h1 className='text-3xl font-bold mb-4'>Take Charge of Your Spending</h1>
+        <p className="mb-8">
+          With SpendSmart, every transaction is an opportunity to make smarter
+          decisions. Our real-time tracking puts the power back in your hands,
+          so you can see the full picture and stay ahead of your spending.
+          <br />
+          Embrace the clarity that comes with detailed breakdowns and embrace a
+          budget that works for you. Whether it&apos;s cutting back on the
+          non-essentials or investing in your future, we&apos;re here to
+          illuminate the path to financial freedom.
+        </p>
+      </section>
+    </div>
+  );
+}
