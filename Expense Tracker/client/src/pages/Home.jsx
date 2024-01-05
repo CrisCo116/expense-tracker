e7e9c2b96c49987c7ce9aa7b40c1bfad1157a6d8
Martin <mxrtinee@user.noreply.github.com>
2024-01-04 18:35:53 -0800

    Updated Home to add link to signup page

--- Expense Tracker/client/src/pages/Home.jsx
+++ Expense Tracker/client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Button from '@mui/material/Button'
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
@@ -9,9 +10,11 @@ export default function Home() {
           The smarter way to budget, save, and achieve your financial goals.
           Take the first steps towards a brighter financial future!
         </p>
-        <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" variant="contained">
-          Create Your Free Account
-        </Button>
+        <Link to="/signup" className="no-underline">
+          <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" variant="contained">
+            Create Your Free Account
+          </Button>
+        </Link>
       </section>
       <section className="text-center p-16">
         <h1 className='text-3xl font-bold mb-4'>Take Charge of Your Spending</h1>
