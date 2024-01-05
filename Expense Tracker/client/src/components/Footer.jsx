2ab7ce5b63a82d08694113e8d4dc784c86e79c41
Martin <mxrtinee@user.noreply.github.com>
2024-01-04 23:04:39 -0800

    Updated Income/Expense.jsx Pages

--- Expense Tracker/client/src/components/Footer.jsx
+++ Expense Tracker/client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="bg-blue-100 mt-8 fixed w-full bottom-0">
       <div className="container mx-auto px-4 py-4">
         <p className="text-gray-600 text-center text-sm">
-          Copyright © 2023. All rights are reserved
+          © 2024 SpendSmart. All Rights Reserved.
         </p>
       </div>
     </footer>
