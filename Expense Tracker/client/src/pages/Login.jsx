5e5bc6e51d562db38fc2fc6c36ca27354ec60689
Chris <138630942+CrisCo116@users.noreply.github.com>
2024-01-04 19:24:06 -0800

    added in logo

--- Expense Tracker/client/src/pages/Login.jsx
+++ Expense Tracker/client/src/pages/Login.jsx
@@ -26,13 +26,14 @@ export default function login() {
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            className="mx-auto"
+            src="./src/assets/DALL·E 2024-01-03 18.30.23 - .png"
             alt="Your Company"
           />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+          <h2 className="mt- mb-0 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to your account
           </h2>
+
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
