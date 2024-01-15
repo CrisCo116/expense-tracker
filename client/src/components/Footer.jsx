4c7a7144a3b61fda1be8921725cf989ef8ea7c12
sefufim <sefufim@outlook.com>
2024-01-14 17:05:20 -0800

    added styling to home page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,15 @@
 export default function Footer() {
   return (
-    <footer className="bg-blue-100 mt-8 fixed w-full bottom-0">
+    <footer className="bg-slate-400 mt-8 fixed w-full bottom-0">
       <div className="container mx-auto px-4 py-4">
         <p className="text-gray-600 text-center text-sm">
           © 2024 SpendSmart. All Rights Reserved.
+        </p>
+        <p className="text-gray-600 text-center text-sm mt-2">
+        <a href="https://www.freepik.com/free-vector/startup-managers-presenting-analyzing-sales-growth-chart-group-workers-with-heap-cash-rocket-bar-diagrams-with-arrow-heap-money_12291285.htm#query=finance&position=5&from_view=keyword&track=sph&uuid=217086c4-9182-4816-a33a-b2ec18826bce">Image by pch.vector</a> on Freepik
+        <a href="https://www.freepik.com/free-vector/piggy-bank-concept-illustration_13283988.htm#query=piggy%20bank&position=1&from_view=search&track=ais&uuid=23984aeb-333d-422c-864d-c91a2351e9d8">Image by storyset</a> on Freepik
+        
+        <a href="https://www.freepik.com/free-vector/wallet-concept-illustration_8174445.htm#query=spending&position=4&from_view=search&track=sph&uuid=61a82556-f228-40fe-bd4a-04bb26a28bb5">Image by storyset</a> on Freepik<a href="https://www.freepik.com/free-vector/investor-with-laptop-monitoring-growth-dividends-trader-sitting-stack-money-investing-capital-analyzing-profit-graphs-vector-illustration-finance-stock-trading-investment_10173124.htm#query=investing&position=1&from_view=search&track=sph&uuid=f314eaa4-6e85-4330-ad2c-0fa3b933ae78">Image by pch.vector</a> on Freepik
         </p>
       </div>
     </footer>
