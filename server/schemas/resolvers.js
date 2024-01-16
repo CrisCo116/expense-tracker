c9055af97657d100f979073d672edaed7d0ce462
birdslisten <justin.schultz37@gmail.com>
2024-01-15 16:03:16 -0800

    last merge did not include getUser query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,37 +4,50 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-      users: async (_, __, context) => {
-        if (context.user) {
-          const user = await User.findOne({ _id: context.user._id });
-          return user;
-        }
-        throw AuthenticationError;
-      },
+        users: async (_, __, context) => {
+            if (context.user) {
+                const user = await User.findOne({ _id: context.user._id });
+                return user;
+            }
+            throw AuthenticationError;
+        },
+        getUser: async (_, __, context) => {
+            try {
+                if (!context.user) {
+                    throw new Error('You must be logged in to fetch user data');
+                }
+
+                const user = await User.findOne({ _id: context.user._id });
+                return user;
+            } catch (error) {
+                console.error(error);
+                throw error;
+            }
+        },
     },
     Mutation: {
         signup: async (parent, { email, password }) => {
-        const user = await User.create({ email, password });
-        const token = signToken(user);
-        return { token, user };
-      },
-      login: async (parent, { email, password }) => {
-        const user = await User.findOne({ email });
-  
-        if (!user) {
-          throw AuthenticationError;
-        }
-  
-        const correctPw = await user.isCorrectPassword(password);
-  
-        if (!correctPw) {
-          throw AuthenticationError;
-        }
-  
-        const token = signToken(user);
-  
-        return { token, user };
-      },
+            const user = await User.create({ email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw AuthenticationError;
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
         signout: (parent, args, context) => {
             return new Promise((resolve, reject) => {
                 context.user = null;
@@ -43,7 +56,7 @@ const resolvers = {
                 });
             });
         },
-      
+
         addIncomeSource: async (_, { source, incomeAmount }, context) => {
             try {
                 if (!context.user) {
@@ -70,10 +83,10 @@ const resolvers = {
 
         addFixedExpense: async (_, { input }) => {
             try {
-              console.log(input)
-              const newFixedExpense = new FixedExpense(input);
-              await newFixedExpense.save();
-              return newFixedExpense;
+                console.log(input)
+                const newFixedExpense = new FixedExpense(input);
+                await newFixedExpense.save();
+                return newFixedExpense;
 
             } catch (error) {
                 console.error(error);
